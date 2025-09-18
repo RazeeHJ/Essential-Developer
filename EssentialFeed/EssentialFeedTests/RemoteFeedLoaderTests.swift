0e3c2829cdca1137e15c695d476192d82f09ad1b
Razee Hussein-Jamal <razee.hussein-jamal@electrolux.com>
2025-09-18 22:32:00 +0200

    Request data from on `load`

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -11,19 +11,35 @@ import XCTest
 struct RemoteFeedLoaderTests {
 
     class RemoteFeedLoader {
-
+        func load() {
+            HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        }
     }
 
     class HTTPClient {
+        static var shared = HTTPClient()
+
+        private init() {}
+
         var requestedURL: URL?
     }
 
     @Test
     func test_init_doesNotRequestDataFromURL()  {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         let _ = RemoteFeedLoader()
 
         XCTAssertNil(client.requestedURL)
+    }
+
+    @Test
+    func test_load_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+
+        sut.load()
+
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
