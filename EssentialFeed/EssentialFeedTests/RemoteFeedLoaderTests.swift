31b0fa9465d59565056ff4d1c8fcee32a994823f
Razee Hussein-Jamal <razee.hussein-jamal@electrolux.com>
2025-09-21 21:05:31 +0200

    Add more test assertions for different HTTP status code

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -56,11 +56,14 @@ struct RemoteFeedLoaderTests {
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
 
-        var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load { capturedErrors.append($0) }
+        let samples = [199, 201, 300, 400, 500]
+        samples.enumerated().forEach { index, code in
+            var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+            client.complete(withStatusCode: code, at: index)
 
-        client.complete(withStatusCode: 400)
-        #expect(capturedErrors == [.invalidData])
+            #expect(capturedErrors == [.invalidData])
+        }
     }
 
     // MARK - Helpers
